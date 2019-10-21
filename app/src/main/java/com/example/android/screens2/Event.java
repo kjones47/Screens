aa483eb904af2da53e00da0bec1390dc50363895
Kenisha Jones <kjones47@binghamton.edu>
2019-10-20 22:51:15 -0400

    person class

---
+++ app/src/main/java/com/example/android/screens2/Event.java
@@ -0,0 +1,16 @@
+package com.example.android.screens2;
+
+public class Event {
+
+    int startTime, endTime;
+    int month, date, year;
+    String title;
+    String chair;
+    String [] attendees;
+    String [] rsvps;
+    int points;
+
+    public Event(){
+
+    }
+}
---
+++ app/src/main/java/com/example/android/screens2/Member.java
@@ -0,0 +1,30 @@
+package com.example.android.screens2;
+
+public class Member {
+    String name, yClass, year, nickname;
+    boolean eboard, chair, active, pnm;
+    Event [] attended;
+    int points;
+    Member big;
+    Member litte;
+
+    public Member(String n, String c, String y, String s){
+        active = true;
+        points = 0;
+    }
+
+    public void setEboard(){
+        eboard = true;
+    }
+    public void setChair(){
+        chair = true;
+    }
+    public void setActive(){
+        active = true;
+    }
+
+    public void addPoints(){
+
+    }
+
+}
